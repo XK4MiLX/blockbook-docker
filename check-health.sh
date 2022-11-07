51524016629b6e44048790da138e65fe526e9d5a
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2022-11-07 22:45:17 +0100

    Update check-health.sh

--- check-health.sh
+++ check-health.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
-
 CURRENT_NODE_HEIGHT=$($COIN-cli -rpcpassword="$RPC_PASS" -rpcuser="$RPC_USER" -getinfo 2>/dev/null | jq .blocks)
 if [[ "$CURRENT_NODE_HEIGHT" == "" ]]; then
   CURRENT_NODE_HEIGHT=$($COIN-cli -rpcpassword="$RPC_PASS" -rpcuser="$RPC_USER" getinfo 2>/dev/null | jq .blocks)
 fi
 
 if ! egrep -o "^[0-9]+$" <<< "$CURRENT_NODE_HEIGHT" &>/dev/null; then
-  echo "Daemon not working correct..."
+  echo -e "Daemon = [FAILED]"
   exit 1
 else
-  curl -sSL  http://localhost:$BLOCKBOOK_PORT/api
+  bloks=$(curl -sSL  http://localhost:$BLOCKBOOK_PORT/api 2>/dev/null | jq -r .backend.blocks)
+  headers=$(curl -sSL  http://localhost:$BLOCKBOOK_PORT/api 2>/dev/null | jq -r .backend.headers)
+  if [[ $bloks != "" && $headers != "" ]]; then
+    diff=$(echo $[($headers-$bloks)])
+    progress=$(awk 'BEGIN {total=ARGV[1] / ARGV[2]; printf("%.2f", total*100)}' $diff $headers)
+    echo -e "Blockbook = [OK], Daemon = [OK], Sync progress: $progress%"
+  else
+    echo -e "Blockbook = [FAILED], Daemon = [OK]"
+    exit 1
+  fi
   exit
 fi
 
