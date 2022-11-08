45d722ff98707cc8b427a33906fd13fe1682861c
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2022-11-08 13:51:39 +0100

    added rpc url protocol variable

--- blockbook.sh
+++ blockbook.sh
@@ -19,10 +19,11 @@ if [[ ! -f /root/config_created ]]; then
   echo -e "Updating blockchaincfg.json..."
   RPC_HOST="${RPC_HOST:-localhost}"
   MQ_PORT="${MQ_URL:-29000}"
+  RPC_URL_PROTOCOL="${RPC_URL_PROTOCOL:-http}
   echo "$(jq -r --arg key "rpc_user" --arg value "$RPC_USER" '.[$key]=$value' $CFG_FILE)" > $CFG_FILE
   echo "$(jq -r --arg key "rpc_pass" --arg value "$RPC_PASS" '.[$key]=$value' $CFG_FILE)" > $CFG_FILE
   echo "$(jq -r --arg key "rpc_timeout" --argjson value 50 '.[$key]=$value' $CFG_FILE)" > $CFG_FILE
-  echo "$(jq -r --arg key "rpc_url" --arg value "http://$RPC_HOST:$RPC_PORT" '.[$key]=$value' $CFG_FILE)" > $CFG_FILE
+  echo "$(jq -r --arg key "rpc_url" --arg value "$RPC_URL_PROTOCOL://$RPC_HOST:$RPC_PORT" '.[$key]=$value' $CFG_FILE)" > $CFG_FILE
   echo "$(jq -r --arg key "message_queue_binding" --arg value "tcp://$RPC_HOST:$MQ_PORT" '.[$key]=$value' $CFG_FILE)" > $CFG_FILE
   echo "Disabled updating blockchaincfg.json..." > /root/config_created
 else
