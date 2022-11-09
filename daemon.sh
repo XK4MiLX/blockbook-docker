eedef4b904c1cbe0c66fd281d792ecb497f574ed
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2022-11-09 10:21:17 +0100

    improved bootstrapping
    - added B_FILE, B_TIMEOUT, B_SERVERS_LIST variables for bootstrap custom solution

--- daemon.sh
+++ daemon.sh
@@ -7,16 +7,6 @@ SEA="\\033[38;5;49m"
 GREEN='\033[1;32m'
 CYAN='\033[1;36m'
 NC='\033[0m'
-#emoji codes
-#CHECK_MARK="${GREEN}\xE2\x9C\x94${NC}"
-#X_MARK="${RED}\xE2\x9C\x96${NC}"
-#PIN="${RED}\xF0\x9F\x93\x8C${NC}"
-#CLOCK="${GREEN}\xE2\x8C\x9B${NC}"
-#ARROW="${SEA}\xE2\x96\xB6${NC}"
-#BOOK="${RED}\xF0\x9F\x93\x8B${NC}"
-#HOT="${ORANGE}\xF0\x9F\x94\xA5${NC}"
-#WORNING="${RED}\xF0\x9F\x9A\xA8${NC}"
-#RIGHT_ANGLE="${GREEN}\xE2\x88\x9F${NC}"
 server_offline="0"
 failed_counter="0"
 
@@ -32,12 +22,12 @@ function cdn_speedtest() {
 	else
 		BOOTSTRAP_FILE="$1"
 	fi
-	if [[ -z $2 ]]; then
+	if [[ -z $2 || "$2" == "0" ]]; then
 		dTime="5"
 	else
 		dTime="$2"
 	fi
-	if [[ -z $3 ]]; then
+	if [[ -z $3 || "$3" == "0" ]]; then
 		rand_by_domain=("5" "6" "7" "8" "9" "10" "11" "12")
 	else
 		msg="$3"
@@ -49,7 +39,7 @@ function cdn_speedtest() {
 	size_list=()
 	i=0
 	len=${#rand_by_domain[@]}
-	echo -e "${ARROW} ${CYAN}Running quick download speed test for ${BOOTSTRAP_FILE}, Servers: ${GREEN}$len${NC}"
+	echo -e "${CYAN}Running quick download speed test for ${BOOTSTRAP_FILE}, Servers: ${GREEN}$len${NC}"
 	start_test=`date +%s`
 	while [ $i -lt $len ];
    do
@@ -62,9 +52,9 @@ function cdn_speedtest() {
 		mb=$(bc <<<"scale=2; $testing_size / 1048576 / $dTime" | awk '{printf "%2.2f\n", $1}')
 		if [[ "$custom_url" == "1" ]]; then
 			domain=$(sed -e 's|^[^/]*//||' -e 's|/.*$||' <<< ${rand_by_domain[$i]})
-			echo -e "  - ${GREEN}URL - ${YELLOW}${domain}${GREEN} - Bits Downloaded: ${YELLOW}$testing_size${NC} ${GREEN}Average speed: ${YELLOW}$mb ${GREEN}MB/s${NC}"
+			echo -e "- ${GREEN}URL - ${YELLOW}${domain}${GREEN} - Bits Downloaded: ${YELLOW}$testing_size${NC} ${GREEN}Average speed: ${YELLOW}$mb ${GREEN}MB/s${NC}"
 		else
-			echo -e "  - ${GREEN}cdn-${YELLOW}${rand_by_domain[$i]}${GREEN} - Bits Downloaded: ${YELLOW}$testing_size${NC} ${GREEN}Average speed: ${YELLOW}$mb ${GREEN}MB/s${NC}"
+			echo -e "- ${GREEN}cdn-${YELLOW}${rand_by_domain[$i]}${GREEN} - Bits Downloaded: ${YELLOW}$testing_size${NC} ${GREEN}Average speed: ${YELLOW}$mb ${GREEN}MB/s${NC}"
 		fi
 		size_list+=($testing_size)
 		if [[ "$testing_size" == "0" ]]; then
@@ -91,7 +81,6 @@ function cdn_speedtest() {
 		BOOTSTRAP_URL="http://cdn-${server_index}.runonflux.io/apps/fluxshare/getfile/"
 	fi
 	DOWNLOAD_URL="${BOOTSTRAP_URL}${BOOTSTRAP_FILE}"
-   #Print the results
 	mb=$(bc <<<"scale=2; $arr_max / 1048576 / $dTime" | awk '{printf "%2.2f\n", $1}')
 	if [[ "$custom_url" == "1" ]]; then
 		domain=$(sed -e 's|^[^/]*//||' -e 's|/.*$||' <<< ${server_index})
@@ -118,21 +107,19 @@ trap stop_script SIGINT SIGTERM
 
 if [[ "$DAEMON" == "1" ]]; then
 echo -e ""
-echo -e "Daemon Luncher v1.0 $(date '+%Y-%m-%d %H:%M:%S')"
+echo -e "Daemon Luncher v1.0 [$(date '+%Y-%m-%d %H:%M:%S')]"
 echo -e "---------------------------------------------------------------------------"
-#Enable Config file
+
 if [[ "$CONFIG" == "1" ]]; then
   if [[ ! -f /root/.$COIN/$COIN.conf ]]; then
     mkdir -p /root/.$COIN
     echo -e "Creating config file..."
     cat <<- EOF > /root/.$COIN/$COIN.conf
-whitelist=127.0.0.1
 txindex=1
 addressindex=1
 timestampindex=1
 spentindex=1
 rpcport=$RPC_PORT
-rpcallowip=127.0.0.1
 rpcuser=$RPC_USER
 rpcpassword=$RPC_PASS
 EOF
@@ -142,23 +129,26 @@ EOF
  fi
 fi
 
-#ONLY FLUX
-if [[ "$BOOTSTRAP" == "1" && "$COIN" == "flux" && ! -f /root/BOOTSTRAP_LOCKED ]]; then
-    cdn_speedtest "0" "6"
+if [[ "$BOOTSTRAP" == "1" && ! -f /root/BOOTSTRAP_LOCKED ]]; then
+
+    B_FILE="${B_FILE:-0}"
+    B_TIMEOUT="${B_TIMEOUT:-6}"
+    B_SERVERS_LIST="${B_SERVERS_LIST:-0}"
+    
+    cdn_speedtest "$B_FILE" "$B_TIMEOUT" "${B_SERVERS_LIST[@]}"
     if [[ "$server_offline" == "1" ]]; then
-      echo -e "${WORNING} ${CYAN}All Bootstrap server offline, operation aborted.. ${NC}" && sleep 1
+      echo -e "${CYAN}All Bootstrap server offline, operation aborted.. ${NC}" && sleep 1
     else
       cd /root
-      echo -e "${ARROW} ${YELLOW}Downloading File: ${GREEN}$DOWNLOAD_URL ${NC}"
+      echo -e "${YELLOW}Downloading File: ${GREEN}$DOWNLOAD_URL ${NC}"
       wget --tries 5 -O $BOOTSTRAP_FILE $DOWNLOAD_URL -q --no-verbose --show-progress --progress=dot:giga > /dev/null 2>&1
-      tar_file_unpack "/root/$BOOTSTRAP_FILE" "/root/.flux"
+      tar_file_unpack "/root/$BOOTSTRAP_FILE" "/root/.$COIN"
       echo -e "Bootstrap [LOCKED]" > BOOTSTRAP_LOCKED
       rm -rf /root/$BOOTSTRAP_FILE
       sleep 2
    fi
  fi
 
-#Enable fetch params
 if [[ "$FETCH_FILE" != "" ]]; then
   if [[ ! -d /root/.zcash-params ]]; then
     echo -e "Installing fetch-params..."
@@ -166,8 +156,7 @@ if [[ "$FETCH_FILE" != "" ]]; then
   fi
 fi
 
-
-if [[ -f /usr/local/bin/${COIN}d ]]; then
+if [[ -f /usr/local/bin/$BINARY_NAME ]]; then
   echo -e "Stopping daemon (START)..."
   if [[ "$BINARY_NAME" == "${COIN}d" ]]; then
     timeout 10 ${COIN}-cli -rpcpassword="$RPC_PASS" -rpcuser="$RPC_USER" stop
@@ -175,7 +164,7 @@ if [[ -f /usr/local/bin/${COIN}d ]]; then
   pkill -9 ${BINARY_NAME}
 fi
 
-if [[ ! -f /usr/local/bin/${COIN}d ]]; then
+if [[ ! -f /usr/local/bin/$BINARY_NAME ]]; then
   echo -e "Downloading daemon ($COIN)..."
   cd /tmp
   mkdir backend
