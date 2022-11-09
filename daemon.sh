55741174aac614e6a2ec9bffb88c25601a0967f2
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2022-11-09 12:13:32 +0100

    disabled colors in output

--- daemon.sh
+++ daemon.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 #color codes
-RED='\033[1;31m'
-YELLOW='\033[1;33m'
-BLUE="\\033[38;5;27m"
-SEA="\\033[38;5;49m"
-GREEN='\033[1;32m'
-CYAN='\033[1;36m'
-NC='\033[0m'
+#RED='\033[1;31m'
+#$YELLOW='\033[1;33m'
+#BLUE="\\033[38;5;27m"
+#SEA="\\033[38;5;49m"
+#GREEN='\033[1;32m'
+#CYAN='\033[1;36m'
+#NC='\033[0m'
 server_offline="0"
 failed_counter="0"
 
