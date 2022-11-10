3a276547772f0869ba3add7afefec7e3151ab713
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2022-11-10 18:10:02 +0100

    added -conf and fix cli_search

--- daemon.sh
+++ daemon.sh
@@ -12,6 +12,7 @@ failed_counter="0"
 
 function cli_search(){
   if [[ "$CLI_NAME" == "" ]]; then
+    echo -e "Searching for CLI binary..."
     CLI_PATH=$(find /usr/local/bin/ -type f -iname "*-cli" | tail -n1)
     if [[ "$CLI_PATH" != "" ]]; then
       CLI_NAME="${CLI_PATH##*/}"
@@ -20,6 +21,7 @@ function cli_search(){
   fi
 }
 
+
 function tar_file_unpack()
 {
     echo -e "${CYAN}Unpacking bootstrap archive file...${NC}"
@@ -27,77 +29,77 @@ function tar_file_unpack()
 }
 
 function cdn_speedtest() {
-	if [[ -z $1 || "$1" == "0" ]]; then
-		BOOTSTRAP_FILE="daemon_bootstrap.tar.gz"
-	else
-		BOOTSTRAP_FILE="$1"
-	fi
-	if [[ -z $2 || "$2" == "0" ]]; then
-		dTime="5"
-	else
-		dTime="$2"
-	fi
-	if [[ -z $3 || "$3" == "0" ]]; then
-		rand_by_domain=("5" "6" "7" "8" "9" "10" "11" "12")
-	else
-		msg="$3"
-		shift
-		shift
-		rand_by_domain=("$@")
-		custom_url="1"
-	fi
-	size_list=()
-	i=0
-	len=${#rand_by_domain[@]}
-	echo -e "${CYAN}Running quick download speed test for ${BOOTSTRAP_FILE}, Servers: ${GREEN}$len${NC}"
-	start_test=`date +%s`
-	while [ $i -lt $len ];
+        if [[ -z $1 || "$1" == "0" ]]; then
+                BOOTSTRAP_FILE="daemon_bootstrap.tar.gz"
+        else
+                BOOTSTRAP_FILE="$1"
+        fi
+        if [[ -z $2 || "$2" == "0" ]]; then
+                dTime="5"
+        else
+                dTime="$2"
+        fi
+        if [[ -z $3 || "$3" == "0" ]]; then
+                rand_by_domain=("5" "6" "7" "8" "9" "10" "11" "12")
+        else
+                msg="$3"
+                shift
+                shift
+                rand_by_domain=("$@")
+                custom_url="1"
+        fi
+        size_list=()
+        i=0
+        len=${#rand_by_domain[@]}
+        echo -e "${CYAN}Running quick download speed test for ${BOOTSTRAP_FILE}, Servers: ${GREEN}$len${NC}"
+        start_test=`date +%s`
+        while [ $i -lt $len ];
    do
-		if [[ "$custom_url" == "1" ]]; then
-			testing=$(curl -m ${dTime} ${rand_by_domain[$i]}${BOOTSTRAP_FILE}  --output testspeed -fail --silent --show-error 2>&1)
-		else
-			testing=$(curl -m ${dTime} http://cdn-${rand_by_domain[$i]}.runonflux.io/apps/fluxshare/getfile/${BOOTSTRAP_FILE}  --output testspeed -fail --silent --show-error 2>&1)
-		fi
-		testing_size=$(grep -Po "\d+" <<< "$testing" | paste - - - - | awk '{printf  "%d\n",$3}')
-		mb=$(bc <<<"scale=2; $testing_size / 1048576 / $dTime" | awk '{printf "%2.2f\n", $1}')
-		if [[ "$custom_url" == "1" ]]; then
-			domain=$(sed -e 's|^[^/]*//||' -e 's|/.*$||' <<< ${rand_by_domain[$i]})
-			echo -e "- ${GREEN}URL - ${YELLOW}${domain}${GREEN} - Bits Downloaded: ${YELLOW}$testing_size${NC} ${GREEN}Average speed: ${YELLOW}$mb ${GREEN}MB/s${NC}"
-		else
-			echo -e "- ${GREEN}cdn-${YELLOW}${rand_by_domain[$i]}${GREEN} - Bits Downloaded: ${YELLOW}$testing_size${NC} ${GREEN}Average speed: ${YELLOW}$mb ${GREEN}MB/s${NC}"
-		fi
-		size_list+=($testing_size)
-		if [[ "$testing_size" == "0" ]]; then
-			failed_counter=$(($failed_counter+1))
-		fi
-		i=$(($i+1))
-	done
-	rServerList=$((${#size_list[@]}-$failed_counter))
-	echo -e "${CYAN}Valid servers: ${GREEN}${rServerList} ${CYAN}- Duration: ${GREEN}$((($(date +%s)-$start_test)/60)) min. $((($(date +%s)-$start_test) % 60)) sec.${NC}"
-	rm -rf testspeed > /dev/null 2>&1
-	if [[ "$rServerList" == "0" ]]; then
-	server_offline="1"
-	return 
-	fi
-	arr_max=$(printf '%s\n' "${size_list[@]}" | sort -n | tail -1)
-	for i in "${!size_list[@]}"; do
-		[[ "${size_list[i]}" == "$arr_max" ]] &&
-		max_indexes+=($i)
-	done
-	server_index=${rand_by_domain[${max_indexes[0]}]}
-	if [[ "$custom_url" == "1" ]]; then
-		BOOTSTRAP_URL="$server_index"
-	else
-		BOOTSTRAP_URL="http://cdn-${server_index}.runonflux.io/apps/fluxshare/getfile/"
-	fi
-	DOWNLOAD_URL="${BOOTSTRAP_URL}${BOOTSTRAP_FILE}"
-	mb=$(bc <<<"scale=2; $arr_max / 1048576 / $dTime" | awk '{printf "%2.2f\n", $1}')
-	if [[ "$custom_url" == "1" ]]; then
-		domain=$(sed -e 's|^[^/]*//||' -e 's|/.*$||' <<< ${server_index})
-		echo -e "${CYAN}Best server is: ${YELLOW}${domain} ${GREEN}Average speed: ${YELLOW}$mb ${GREEN}MB/s${NC}"
-	else
-		echo -e "${CYAN}Best server is: ${GREEN}cdn-${YELLOW}${rand_by_domain[${max_indexes[0]}]} ${GREEN}Average speed: ${YELLOW}$mb ${GREEN}MB/s${NC}"
-	fi
+                if [[ "$custom_url" == "1" ]]; then
+                        testing=$(curl -m ${dTime} ${rand_by_domain[$i]}${BOOTSTRAP_FILE}  --output testspeed -fail --silent --show-error 2>&1)
+                else
+                        testing=$(curl -m ${dTime} http://cdn-${rand_by_domain[$i]}.runonflux.io/apps/fluxshare/getfile/${BOOTSTRAP_FILE}  --output testspeed -fail --silent --show-error 2>&1)
+                fi
+                testing_size=$(grep -Po "\d+" <<< "$testing" | paste - - - - | awk '{printf  "%d\n",$3}')
+                mb=$(bc <<<"scale=2; $testing_size / 1048576 / $dTime" | awk '{printf "%2.2f\n", $1}')
+                if [[ "$custom_url" == "1" ]]; then
+                        domain=$(sed -e 's|^[^/]*//||' -e 's|/.*$||' <<< ${rand_by_domain[$i]})
+                        echo -e "- ${GREEN}URL - ${YELLOW}${domain}${GREEN} - Bits Downloaded: ${YELLOW}$testing_size${NC} ${GREEN}Average speed: ${YELLOW}$mb ${GREEN}MB/s${NC}"
+                else
+                        echo -e "- ${GREEN}cdn-${YELLOW}${rand_by_domain[$i]}${GREEN} - Bits Downloaded: ${YELLOW}$testing_size${NC} ${GREEN}Average speed: ${YELLOW}$mb ${GREEN}MB/s${NC}"
+                fi
+                size_list+=($testing_size)
+                if [[ "$testing_size" == "0" ]]; then
+                        failed_counter=$(($failed_counter+1))
+                fi
+                i=$(($i+1))
+        done
+        rServerList=$((${#size_list[@]}-$failed_counter))
+        echo -e "${CYAN}Valid servers: ${GREEN}${rServerList} ${CYAN}- Duration: ${GREEN}$((($(date +%s)-$start_test)/60)) min. $((($(date +%s)-$start_test) % 60)) sec.${NC}"
+        rm -rf testspeed > /dev/null 2>&1
+        if [[ "$rServerList" == "0" ]]; then
+        server_offline="1"
+        return
+        fi
+        arr_max=$(printf '%s\n' "${size_list[@]}" | sort -n | tail -1)
+        for i in "${!size_list[@]}"; do
+                [[ "${size_list[i]}" == "$arr_max" ]] &&
+                max_indexes+=($i)
+        done
+        server_index=${rand_by_domain[${max_indexes[0]}]}
+        if [[ "$custom_url" == "1" ]]; then
+                BOOTSTRAP_URL="$server_index"
+        else
+                BOOTSTRAP_URL="http://cdn-${server_index}.runonflux.io/apps/fluxshare/getfile/"
+        fi
+        DOWNLOAD_URL="${BOOTSTRAP_URL}${BOOTSTRAP_FILE}"
+        mb=$(bc <<<"scale=2; $arr_max / 1048576 / $dTime" | awk '{printf "%2.2f\n", $1}')
+        if [[ "$custom_url" == "1" ]]; then
+                domain=$(sed -e 's|^[^/]*//||' -e 's|/.*$||' <<< ${server_index})
+                echo -e "${CYAN}Best server is: ${YELLOW}${domain} ${GREEN}Average speed: ${YELLOW}$mb ${GREEN}MB/s${NC}"
+        else
+                echo -e "${CYAN}Best server is: ${GREEN}cdn-${YELLOW}${rand_by_domain[${max_indexes[0]}]} ${GREEN}Average speed: ${YELLOW}$mb ${GREEN}MB/s${NC}"
+        fi
 }
 
 if [[ "$CONFIG_DIR" == "" ]]; then
@@ -109,8 +111,8 @@ stop_script() {
   if [[ "$CLI_NAME" != "" ]]; then
     timeout 10 ${CLI_NAME} -rpcpassword="$RPC_PASS" -rpcuser="$RPC_USER" stop
   fi
-  if [[ -f /root/daemon_config.json ]]; then
-    pkill -9 $(jq .binary_name /root/daemon_config.json)
+  if [[ "$BINARY_NAME" != "" ]]; then
+    pkill -9 $BINARY_NAME
   fi
   exit 0
 }
@@ -156,7 +158,7 @@ if [[ "$BOOTSTRAP" == "1" && ! -f /root/BOOTSTRAP_LOCKED ]]; then
     B_FILE="${B_FILE:-0}"
     B_TIMEOUT="${B_TIMEOUT:-6}"
     B_SERVERS_LIST="${B_SERVERS_LIST:-0}"
-    
+
     cdn_speedtest "$B_FILE" "$B_TIMEOUT" "${B_SERVERS_LIST[@]}"
     if [[ "$server_offline" == "1" ]]; then
       echo -e "${CYAN}All Bootstrap server offline, operation aborted.. ${NC}" && sleep 1
@@ -209,14 +211,6 @@ if [[ ! -f /usr/local/bin/$BINARY_NAME ]]; then
     if [[ ! -f /root/daemon_config.json ]]; then
       echo "{}" > /root/daemon_config.json
       echo "$(jq -r --arg key "binary_name" --arg value "$BINARY_NAME" '.[$key]=$value' /root/daemon_config.json)" > /root/daemon_config.json
-      cli_search
-    fi
-  else
-    if [[ "$CLI_NAME" == "" ]]; then 
-      if [[ ! -f /root/daemon_config.json ]]; then
-        echo "{}" > /root/daemon_config.json
-      fi
-      cli_search
     fi
   fi
   echo -e "BINARY URL: $DAEMON_URL"
@@ -226,6 +220,12 @@ if [[ ! -f /usr/local/bin/$BINARY_NAME ]]; then
   echo -e "Installing daemon ($COIN)..."
   install -m 0755 -o root -g root -t /usr/local/bin backend/*
   rm -rf /tmp/*
+  if [[ "$CLI_NAME" == "" ]]; then
+    if [[ ! -f /root/daemon_config.json ]]; then
+        echo "{}" > /root/daemon_config.json
+    fi
+    cli_search
+  fi
 fi
 
 cd /
@@ -235,7 +235,7 @@ if [[ "$CONFIG" == "0" || "$CONFIG" == "" ]]; then
   ${BINARY_NAME} ${CLIFLAGS}
 else
   echo -e "Starting $COIN daemon (Config: ENABLED)..."
-  ${BINARY_NAME} -datadir="/root/.${COIN}"
+  ${BINARY_NAME} -datadir="/root/.${COIN}" -conf="/root/.${COIN}/${COIN}.conf"
 fi
 else
   echo -e "Daemon Luncher [DISABLED]..."
