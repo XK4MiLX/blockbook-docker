48e9c7c2b9a337221af8c5b56b8cd18e45434b8d
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2022-11-10 21:35:36 +0100

    disabled install logs

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 if [[ ! -d /root/blockbook ]]; then
   echo -e "Installing RocksDB..."
-  cd /root && git clone -b $ROCKSDB_VERSION --depth 1 https://github.com/facebook/rocksdb.git
-  cd /root/rocksdb && CFLAGS=-fPIC CXXFLAGS=-fPIC make -j 4 release
+  cd /root && git clone -b $ROCKSDB_VERSION --depth 1 https://github.com/facebook/rocksdb.git > /dev/null 2>&1 
+  cd /root/rocksdb && CFLAGS=-fPIC CXXFLAGS=-fPIC make -j 4 release > /dev/null 2>&1 
   # Install ZeroMQ
   #echo -e "Installing ZeroMQ..."
   #cd /root && git clone https://github.com/zeromq/libzmq && \
@@ -19,22 +19,22 @@ if [[ ! -d /root/blockbook ]]; then
     BLOCKBOOKGIT_URL=https://github.com/trezor/blockbook.git
   fi
   
-  cd /root && git clone $BLOCKBOOKGIT_URL && \
+  cd /root && git clone $BLOCKBOOKGIT_URL > /dev/null 2>&1 && \
   cd /root/blockbook && \
-  go mod download && \
+  go mod download > /dev/null 2>&1  && \
   BUILDTIME=$(date --iso-8601=seconds); \
   GITCOMMIT=$(git describe --always --dirty); \
   LDFLAGS="-X github.com/trezor/blockbook/common.version=${TAG} -X github.com/trezor/blockbook/common.gitcommit=${GITCOMMIT} -X github.com/trezor/blockbook/common.buildtime=${BUILDTIME}" && \
-  go build -tags rocksdb_6_16 -ldflags="-s -w ${LDFLAGS}"
+  go build -tags rocksdb_6_16 -ldflags="-s -w ${LDFLAGS}" > /dev/null 2>&1 
   echo -e "Build: $BUILDTIME, Commit: $GITCOMMIT, Version: $TAG"
   if [[ -f /root/blockbook/blockbook ]]; then
     echo -e "Blockbook build [OK]..."
   else
     echo -e "Blockbook build [FAILED]..."
     echo -e "Cleaning..."
-    rm -rf /root/blockbook
-    rm -rf /root/libzmq
-    rm -rf /root/rocksdb
+    rm -rf /root/blockbook > /dev/null 2>&1 
+    rm -rf /root/libzmq > /dev/null 2>&1 
+    rm -rf /root/rocksdb > /dev/null 2>&1 
     exit 1
   fi
   echo -e "Creating blockchaincfg.sh for $COIN..."
