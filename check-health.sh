cecb358569a69aa76eec809e3878f3a421f88109
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2022-11-10 12:44:23 +0100

    added -datadir

--- check-health.sh
+++ check-health.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-CURRENT_NODE_HEIGHT=$($COIN-cli -rpcpassword="$RPC_PASS" -rpcuser="$RPC_USER" -getinfo 2>/dev/null | jq .blocks)
+CURRENT_NODE_HEIGHT=$($COIN-cli -datadir="/root/.$COIN" -rpcpassword="$RPC_PASS" -rpcuser="$RPC_USER" -getinfo 2>/dev/null | jq .blocks)
 if [[ "$CURRENT_NODE_HEIGHT" == "" ]]; then
-  CURRENT_NODE_HEIGHT=$($COIN-cli -rpcpassword="$RPC_PASS" -rpcuser="$RPC_USER" getinfo 2>/dev/null | jq .blocks)
+  CURRENT_NODE_HEIGHT=$($COIN-cli -datadir="/root/.$COIN" -rpcpassword="$RPC_PASS" -rpcuser="$RPC_USER" getinfo 2>/dev/null | jq .blocks)
 fi
 
 if ! egrep -o "^[0-9]+$" <<< "$CURRENT_NODE_HEIGHT" &>/dev/null; then
