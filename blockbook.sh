d393fae5de61500d5f6aea0f867bc8f17ec30745
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2022-11-11 10:30:21 +0100

    disabled MQ_PORT

--- blockbook.sh
+++ blockbook.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 TRY=1
-echo -e ""
-echo -e "Blockbook Luncher v1.0 [$(date '+%Y-%m-%d %H:%M:%S')]"
-echo -e "---------------------------------------------------------------------------"
-echo -e "Blockbook Settings: COIN=$COIN, RPC_USER=$RPC_USER, RPC_PASS=$RPC_PASS, RPC_PORT=$RPC_PORT, BLOCKBOOK_PORT=$BLOCKBOOK_PORT"
+echo -e "| Blockbook Luncher v1.0 [$(date '+%Y-%m-%d %H:%M:%S')]"
+echo -e "| ---------------------------------------------------------------------------"
+echo -e "| Blockbook Settings: COIN=$COIN, RPC_USER=$RPC_USER, RPC_PASS=$RPC_PASS, RPC_PORT=$RPC_PORT, BLOCKBOOK_PORT=$BLOCKBOOK_PORT"
 while true; do
-   echo -e "Awaiting for Blockbook build...($TRY)"
+   echo -e "| Awaiting for Blockbook build...($TRY)"
    if [[ -d /root/blockbook ]]; then
      sleep 20
      break
@@ -20,20 +19,21 @@ fi
 
 CFG_FILE=/root/blockbook/build/blockchaincfg.json
 if [[ ! -f /root/CONFIG_CRETED ]]; then
-  echo -e "Updating blockchaincfg.json..."
+  echo -e "| Updating blockchaincfg.json..."
   RPC_HOST="${RPC_HOST:-localhost}"
-  MQ_PORT="${MQ_URL:-29000}"
+  #MQ_PORT="${MQ_URL:-29000}"
   RPC_URL_PROTOCOL="${RPC_URL_PROTOCOL:-http}"
   echo "$(jq -r --arg key "rpc_user" --arg value "$RPC_USER" '.[$key]=$value' $CFG_FILE)" > $CFG_FILE
   echo "$(jq -r --arg key "rpc_pass" --arg value "$RPC_PASS" '.[$key]=$value' $CFG_FILE)" > $CFG_FILE
   echo "$(jq -r --arg key "rpc_timeout" --argjson value 50 '.[$key]=$value' $CFG_FILE)" > $CFG_FILE
   echo "$(jq -r --arg key "rpc_url" --arg value "$RPC_URL_PROTOCOL://$RPC_HOST:$RPC_PORT" '.[$key]=$value' $CFG_FILE)" > $CFG_FILE
-  echo "$(jq -r --arg key "message_queue_binding" --arg value "tcp://$RPC_HOST:$MQ_PORT" '.[$key]=$value' $CFG_FILE)" > $CFG_FILE
+  #echo "$(jq -r --arg key "message_queue_binding" --arg value "tcp://$RPC_HOST:$MQ_PORT" '.[$key]=$value' $CFG_FILE)" > $CFG_FILE
   echo "Disabled updating blockchaincfg.json..." > /root/CONFIG_CRETED
 else
-  echo -e "Blockchaincfg.json [LOCKED]..."
+  echo -e "| Blockchaincfg.json [LOCKED]..."
 fi
 
 cd /root/blockbook
-echo -e "Starting Blockbook ($COIN)..."
+echo -e "| Starting Blockbook ($COIN)..."
 exec ./blockbook -sync -blockchaincfg=$CFG_FILE -debug -workers=${WORKERS:-1} -public=:${BLOCKBOOK_PORT} -logtostderr
+echo -e "| ---------------------------------------------------------------------------"
