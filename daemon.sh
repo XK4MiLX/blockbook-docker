0ad8f061b81d8d25b7c69912f4f1e5b6a1076b70
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2022-11-11 10:25:37 +0100

    added more logs

--- daemon.sh
+++ daemon.sh
@@ -12,7 +12,7 @@ failed_counter="0"
 
 function cli_search(){
   if [[ "$CLI_NAME" == "" ]]; then
-    echo -e "Searching for CLI binary..."
+    echo -e "| Searching for CLI binary..."
     CLI_PATH=$(find /usr/local/bin/ -type f -iname "*-cli" | tail -n1)
     if [[ "$CLI_PATH" != "" ]]; then
       CLI_NAME="${CLI_PATH##*/}"
@@ -24,7 +24,7 @@ function cli_search(){
 
 function tar_file_unpack()
 {
-    echo -e "${CYAN}Unpacking bootstrap archive file...${NC}"
+    echo -e "| ${CYAN}Unpacking bootstrap archive file...${NC}"
     pv $1 | tar -zx -C $2
 }
 
@@ -51,7 +51,7 @@ function cdn_speedtest() {
         size_list=()
         i=0
         len=${#rand_by_domain[@]}
-        echo -e "${CYAN}Running quick download speed test for ${BOOTSTRAP_FILE}, Servers: ${GREEN}$len${NC}"
+        echo -e "| ${CYAN}Running quick download speed test for ${BOOTSTRAP_FILE}, Servers: ${GREEN}$len${NC}"
         start_test=`date +%s`
         while [ $i -lt $len ];
    do
@@ -64,9 +64,9 @@ function cdn_speedtest() {
                 mb=$(bc <<<"scale=2; $testing_size / 1048576 / $dTime" | awk '{printf "%2.2f\n", $1}')
                 if [[ "$custom_url" == "1" ]]; then
                         domain=$(sed -e 's|^[^/]*//||' -e 's|/.*$||' <<< ${rand_by_domain[$i]})
-                        echo -e "- ${GREEN}URL - ${YELLOW}${domain}${GREEN} - Bits Downloaded: ${YELLOW}$testing_size${NC} ${GREEN}Average speed: ${YELLOW}$mb ${GREEN}MB/s${NC}"
+                        echo -e "| - ${GREEN}URL - ${YELLOW}${domain}${GREEN} - Bits Downloaded: ${YELLOW}$testing_size${NC} ${GREEN}Average speed: ${YELLOW}$mb ${GREEN}MB/s${NC}"
                 else
-                        echo -e "- ${GREEN}cdn-${YELLOW}${rand_by_domain[$i]}${GREEN} - Bits Downloaded: ${YELLOW}$testing_size${NC} ${GREEN}Average speed: ${YELLOW}$mb ${GREEN}MB/s${NC}"
+                        echo -e "| - ${GREEN}cdn-${YELLOW}${rand_by_domain[$i]}${GREEN} - Bits Downloaded: ${YELLOW}$testing_size${NC} ${GREEN}Average speed: ${YELLOW}$mb ${GREEN}MB/s${NC}"
                 fi
                 size_list+=($testing_size)
                 if [[ "$testing_size" == "0" ]]; then
@@ -75,7 +75,7 @@ function cdn_speedtest() {
                 i=$(($i+1))
         done
         rServerList=$((${#size_list[@]}-$failed_counter))
-        echo -e "${CYAN}Valid servers: ${GREEN}${rServerList} ${CYAN}- Duration: ${GREEN}$((($(date +%s)-$start_test)/60)) min. $((($(date +%s)-$start_test) % 60)) sec.${NC}"
+        echo -e "| ${CYAN}Valid servers: ${GREEN}${rServerList} ${CYAN}- Duration: ${GREEN}$((($(date +%s)-$start_test)/60)) min. $((($(date +%s)-$start_test) % 60)) sec.${NC}"
         rm -rf testspeed > /dev/null 2>&1
         if [[ "$rServerList" == "0" ]]; then
         server_offline="1"
@@ -96,9 +96,9 @@ function cdn_speedtest() {
         mb=$(bc <<<"scale=2; $arr_max / 1048576 / $dTime" | awk '{printf "%2.2f\n", $1}')
         if [[ "$custom_url" == "1" ]]; then
                 domain=$(sed -e 's|^[^/]*//||' -e 's|/.*$||' <<< ${server_index})
-                echo -e "${CYAN}Best server is: ${YELLOW}${domain} ${GREEN}Average speed: ${YELLOW}$mb ${GREEN}MB/s${NC}"
+                echo -e "| ${CYAN}Best server is: ${YELLOW}${domain} ${GREEN}Average speed: ${YELLOW}$mb ${GREEN}MB/s${NC}"
         else
-                echo -e "${CYAN}Best server is: ${GREEN}cdn-${YELLOW}${rand_by_domain[${max_indexes[0]}]} ${GREEN}Average speed: ${YELLOW}$mb ${GREEN}MB/s${NC}"
+                echo -e "| ${CYAN}Best server is: ${GREEN}cdn-${YELLOW}${rand_by_domain[${max_indexes[0]}]} ${GREEN}Average speed: ${YELLOW}$mb ${GREEN}MB/s${NC}"
         fi
 }
 
@@ -107,7 +107,7 @@ if [[ "$CONFIG_DIR" == "" ]]; then
 fi
 
 stop_script() {
-  echo -e "Stopping daemon (EXIT)..."
+  echo -e "| Stopping daemon (EXIT)..."
   if [[ "$CLI_NAME" != "" ]]; then
     timeout 10 ${CLI_NAME} -rpcpassword="$RPC_PASS" -rpcuser="$RPC_USER" stop
   fi
@@ -120,12 +120,11 @@ stop_script() {
 trap stop_script SIGINT SIGTERM
 
 if [[ "$DAEMON" == "1" ]]; then
-echo -e ""
-echo -e "Daemon Luncher v1.0 [$(date '+%Y-%m-%d %H:%M:%S')]"
+echo -e "| Daemon Luncher v1.0 [$(date '+%Y-%m-%d %H:%M:%S')]"
 echo -e "---------------------------------------------------------------------------"
 
 if [[ -f /root/daemon_config.json ]]; then
-  echo -e "Loading daemon_config.json..."
+  echo -e "| Loading daemon_config.json..."
   if [[ "$CLI_NAME" == "" ]]; then
     CLI_NAME=$(jq -r .cli_name /root/daemon_config.json)
   fi
@@ -137,7 +136,7 @@ fi
 if [[ "$CONFIG" == "1" ]]; then
   if [[ ! -f /root/.$CONFIG_DIR/$COIN.conf ]]; then
     mkdir -p /root/.$CONFIG_DIR
-    echo -e "Creating config file..."
+    echo -e "| Creating config file..."
     cat <<- EOF > /root/.$CONFIG_DIR/$COIN.conf
 txindex=1
 addressindex=1
@@ -161,10 +160,10 @@ if [[ "$BOOTSTRAP" == "1" && ! -f /root/BOOTSTRAP_LOCKED ]]; then
 
     cdn_speedtest "$B_FILE" "$B_TIMEOUT" "${B_SERVERS_LIST[@]}"
     if [[ "$server_offline" == "1" ]]; then
-      echo -e "${CYAN}All Bootstrap server offline, operation aborted.. ${NC}" && sleep 1
+      echo -e "| ${CYAN}All Bootstrap server offline, operation aborted.. ${NC}" && sleep 1
     else
       cd /root
-      echo -e "${YELLOW}Downloading File: ${GREEN}$DOWNLOAD_URL ${NC}"
+      echo -e "| ${YELLOW}Downloading File: ${GREEN}$DOWNLOAD_URL ${NC}"
       wget --tries 5 -O $BOOTSTRAP_FILE $DOWNLOAD_URL -q --no-verbose --show-progress --progress=dot:giga > /dev/null 2>&1
       tar_file_unpack "/root/$BOOTSTRAP_FILE" "/root/.$CONFIG_DIR"
       echo -e "Bootstrap [LOCKED]" > BOOTSTRAP_LOCKED
@@ -175,13 +174,13 @@ if [[ "$BOOTSTRAP" == "1" && ! -f /root/BOOTSTRAP_LOCKED ]]; then
 
 if [[ "$FETCH_FILE" != "" ]]; then
   if [[ ! -d /root/.zcash-params ]]; then
-    echo -e "Installing fetch-params..."
+    echo -e "| Installing fetch-params..."
     bash -c "$FETCH_FILE" > /dev/null 2>&1 && sleep 2
   fi
 fi
 
 if [[ "$BINARY_NAME" != "" ]]; then
-  echo -e "Stopping daemon (START)..."
+  echo -e "| Stopping daemon (START)..."
   if [[ "$CLI_NAME" != "" ]]; then
     timeout 10 ${CLI_NAME} -rpcpassword="$RPC_PASS" -rpcuser="$RPC_USER" stop
   fi
@@ -189,21 +188,24 @@ if [[ "$BINARY_NAME" != "" ]]; then
 fi
 
 if [[ ! -f /usr/local/bin/$BINARY_NAME ]]; then
-  echo -e "Downloading daemon ($COIN)..."
+  echo -e "| Downloading daemon ($COIN)..."
   cd /tmp
   mkdir backend
-  echo -e "Fetching blockbook config..."
+  echo -e "| Fetching Blockbook config for $COIN..."
   if [[ "$BLOCKBOOKGIT_URL" == "" ]]; then
     BLOCKBOOKGIT_URL="https://github.com/trezor/blockbook.git"
   fi
   
+  echo -e "| GITHUB URL: $BLOCKBOOKGIT_URL"
   re="^(https|git)(:\/\/|@)([^\/:]+)[\/:]([^\/:]+)\/(.+)(.git)*$"
   if [[ $BLOCKBOOKGIT_URL =~ $re ]]; then
     user=${BASH_REMATCH[4]}
     repo=$(cut -d "." -f 1 <<< ${BASH_REMATCH[5]})
   fi
   
-  BLOCKBOOKCONFIG=$(curl -SsL https://raw.githubusercontent.com/$user/$repo/master/configs/coins/${COIN}.json 2>/dev/null | jq .)
+  RAW_CONF_URL="https://raw.githubusercontent.com/$user/$repo/master/configs/coins/${COIN}.json"
+  echo -e "| CONFIG URL: $RAW_CONF_URL"
+  BLOCKBOOKCONFIG=$(curl -SsL $RAW_CONF_URL 2>/dev/null | jq .)
   if [[ "$DAEMON_URL" == "" ]]; then
       DAEMON_URL=$(jq -r .backend.binary_url <<< "$BLOCKBOOKCONFIG")
   fi
@@ -223,11 +225,11 @@ if [[ ! -f /usr/local/bin/$BINARY_NAME ]]; then
       echo "$(jq -r --arg key "binary_name" --arg value "$BINARY_NAME" '.[$key]=$value' /root/daemon_config.json)" > /root/daemon_config.json
     fi
   fi
-  echo -e "BINARY URL: $DAEMON_URL"
+  echo -e "| BINARY URL: $DAEMON_URL"
   wget -q --show-progress -c -t 5 $DAEMON_URL
   strip_lvl=$(tar -tvf ${DAEMON_URL##*/} | grep ${BINARY_NAME}$ | awk '{ printf "%s\n", $6 }' | awk -F\/ '{print NF-1}')
   tar --exclude="share" --exclude="lib" --exclude="include" -C backend --strip $strip_lvl -xf ${DAEMON_URL##*/}
-  echo -e "Installing daemon ($COIN)..."
+  echo -e "| Installing daemon ($COIN)..."
   install -m 0755 -o root -g root -t /usr/local/bin backend/*
   rm -rf /tmp/*
   if [[ "$CLI_NAME" == "" ]]; then
@@ -241,13 +243,15 @@ fi
 cd /
 sleep 5
 if [[ "$CONFIG" == "0" || "$CONFIG" == "" ]]; then
-  echo -e "Starting $COIN daemon (Config: DISABLED)..."
+  echo -e "| Starting $COIN daemon (Config: DISABLED)..."
   ${BINARY_NAME} ${CLIFLAGS}
 else
-  echo -e "Starting $COIN daemon (Config: ENABLED)..."
+  echo -e "| Starting $COIN daemon (Config: ENABLED)..."
   ${BINARY_NAME} -datadir="/root/.${COIN}" -conf="/root/.${COIN}/${COIN}.conf"
 fi
+echo -e "---------------------------------------------------------------------------"
 else
-  echo -e "Daemon Luncher [DISABLED]..."
+  echo -e "| Daemon Luncher [DISABLED]..."
+  echo -e "---------------------------------------------------------------------------"
   exit
 fi
