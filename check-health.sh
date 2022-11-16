129d1863344434ad9cc692f1de1fd5cf2efc93a8
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2022-11-16 21:52:30 +0100

    healt check improvement

--- check-health.sh
+++ check-health.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
-if [[ "$CONFIG_DIR" == "" ]]; then
-  CONFIG_DIR=$COIN
-fi
+ CONFIG_FILE=${CONFIG_FILE:-$COIN}
+ CONFIG_DIR=${CONFIG_DIR:-$COIN}
+ ### BlockBook checking
+  blockbookapi=$(curl -sSL  http://localhost:$BLOCKBOOK_PORT/api 2>/dev/null | jq .)
+  bloks=$(jq -r .backend.blocks <<< "$blockbookapi")
+  headers=$(jq -r .backend.headers <<< "$blockbookapi")
+  blockbook=$(jq -r .blockbook.bestHeight <<< "$blockbookapi")
+  if [[ $bloks != "" && $blockbook != "" ]]; then  
+    if [[ $headers != "" ]]; then
+      progress1=$(awk 'BEGIN {total=ARGV[1] / ARGV[2]; printf("%.2f", total*100)}' $bloks $headers)
+      progress2=$(awk 'BEGIN {total=ARGV[1] / ARGV[2]; printf("%.2f", total*100)}' $blockbook $headers)
+      msg="Blockbook = [OK], Daemon = [OK], Daemon Sync: ${progress1}%, Blockbook Sync: ${progress2}%"
+    else
+      msg="Blockbook = [OK], Daemon = [OK], Daemon Height: ${bloks}, Blockbook Height: ${blockbook}%"
+    fi
+    echo -e "${msg}"
+    exit
+  else
+    msg="Blockbook = [FAILED]"
+  fi
 
 if [[ "$CLI_NAME" == "" ]]; then
   if [[ -f /root/daemon_config.json ]]; then
@@ -11,14 +28,15 @@ if [[ "$CLI_NAME" == "" ]]; then
 fi
 
 if [[ "$CLI_NAME" == "" ]]; then
-  echo -e "HEALCHECK [DISABLED]"
+  msg="$msg, Daemon = [HEALCHECK DISABLED]"
+  echo -e "$msg"
   exit 1
 fi
-
-if [[ -f /root/${CONFIG_DIR}/${COIN}.conf ]]; then
-  CURRENT_NODE_HEIGHT=$(${CLI_NAME} -conf="/root/${CONFIG_DIR}/${COIN}.conf" -getinfo 2>/dev/null | jq .blocks)
+## Checking Daemon
+if [[ -f /root/${CONFIG_DIR}/${CONFIG_FILE}.conf ]]; then
+  CURRENT_NODE_HEIGHT=$(${CLI_NAME} -conf="/root/${CONFIG_DIR}/${CONFIG_FILE}.conf" -getinfo 2>/dev/null | jq .blocks)
   if [[ "$CURRENT_NODE_HEIGHT" == "" ]]; then
-    CURRENT_NODE_HEIGHT=$(${CLI_NAME} -conf="/root/${CONFIG_DIR}/${COIN}.conf" getinfo 2>/dev/null | jq .blocks)
+    CURRENT_NODE_HEIGHT=$(${CLI_NAME} -conf="/root/${CONFIG_DIR}/${CONFIG_FILE}.conf" getinfo 2>/dev/null | jq .blocks)
   fi
 else
   CURRENT_NODE_HEIGHT=$(${CLI_NAME} -rpcpassword="$RPC_PASS" -rpcuser="$RPC_USER" -getinfo 2>/dev/null | jq .blocks)
@@ -27,22 +45,10 @@ else
   fi
 fi
 
-
 if ! egrep -o "^[0-9]+$" <<< "$CURRENT_NODE_HEIGHT" &>/dev/null; then
-  echo -e "Daemon = [FAILED]"
+  msg="$msg, Daemon = [FAILED]"
   exit 1
 else
-  blockbookapi=$(curl -sSL  http://localhost:$BLOCKBOOK_PORT/api 2>/dev/null | jq .)
-  bloks=$(jq -r .backend.blocks <<< "$blockbookapi")
-  headers=$(jq -r .backend.headers <<< "$blockbookapi")
-  blockbook=$(jq -r .blockbook.bestHeight <<< "$blockbookapi")
-  if [[ $bloks != "" && $headers != "" ]]; then
-    progress1=$(awk 'BEGIN {total=ARGV[1] / ARGV[2]; printf("%.2f", total*100)}' $bloks $headers)
-    progress2=$(awk 'BEGIN {total=ARGV[1] / ARGV[2]; printf("%.2f", total*100)}' $blockbook $headers)
-    echo -e "Blockbook = [OK], Daemon = [OK], Daemon Sync: $progress1%, Blockbook Sync: $progress2%"
-  else
-    echo -e "Blockbook = [FAILED], Daemon = [OK]"
-    exit 1
-  fi
-  exit
+  msg="$msg, Daemon = [OK]"
+  exit 1
 fi
