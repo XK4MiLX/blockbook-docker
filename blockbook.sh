4b734857f9d880135e3e1ac343cb40e931048518
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2022-11-18 12:25:42 +0100

    Cleanup

--- blockbook.sh
+++ blockbook.sh
@@ -12,27 +12,22 @@ while true; do
    sleep 180
    ((TRY=TRY+1))
 done
-
 if [[ "$BOOTSTRAP" == "1" && ! -f /root/BOOTSTRAP_LOCKED ]]; then
   sleep 180
 fi
-
 CFG_FILE=/root/blockbook/build/blockchaincfg.json
 if [[ ! -f /root/CONFIG_CRETED ]]; then
   echo -e "| Updating blockchaincfg.json..."
   RPC_HOST="${RPC_HOST:-localhost}"
-  #MQ_PORT="${MQ_URL:-29000}"
   RPC_URL_PROTOCOL="${RPC_URL_PROTOCOL:-http}"
   echo "$(jq -r --arg key "rpc_user" --arg value "$RPC_USER" '.[$key]=$value' $CFG_FILE)" > $CFG_FILE
   echo "$(jq -r --arg key "rpc_pass" --arg value "$RPC_PASS" '.[$key]=$value' $CFG_FILE)" > $CFG_FILE
   echo "$(jq -r --arg key "rpc_timeout" --argjson value 50 '.[$key]=$value' $CFG_FILE)" > $CFG_FILE
   echo "$(jq -r --arg key "rpc_url" --arg value "$RPC_URL_PROTOCOL://$RPC_HOST:$RPC_PORT" '.[$key]=$value' $CFG_FILE)" > $CFG_FILE
-  #echo "$(jq -r --arg key "message_queue_binding" --arg value "tcp://$RPC_HOST:$MQ_PORT" '.[$key]=$value' $CFG_FILE)" > $CFG_FILE
   echo "Disabled updating blockchaincfg.json..." > /root/CONFIG_CRETED
 else
   echo -e "| Blockchaincfg.json [LOCKED]..."
 fi
-
 cd /root/blockbook
 echo -e "| Starting Blockbook ($COIN)..."
 exec ./blockbook -sync -blockchaincfg=$CFG_FILE -debug -workers=${WORKERS:-1} -public=:${BLOCKBOOK_PORT} -logtostderr
